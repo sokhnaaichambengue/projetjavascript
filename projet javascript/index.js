2fc1a8bb5d65d36815fd903fbea28603dc5e2523
sokhnaaichambengue <151847009+sokhnaaichambengue@users.noreply.github.com>
2024-02-26 09:12:48 +0000

    Sohna aicha mbengue projetjavascript

---
+++ projet javascript/index.js
@@ -0,0 +1,36 @@
+var douleur=prompt("donne les differents maladies")
+if(douleur==="abdomen"){
+    alert("apendicite");
+}
+else if(douleur==="gorge"){
+    alert("fievre");
+}
+else if(douleur==="aucune"){
+    alert("toux");
+}
+
+else{
+    alert("pas de maladie");
+}
+ var fievre=prompt("donne le fievre");
+if(fievre==="oui"){
+    alert("Rhume");
+}
+else{
+    alert("Mal gorge");
+}
+var toux=prompt("donner ses manifestations");
+if(toux==="oui"){
+    alert("fievre")
+}
+else{
+    alert("rien");
+}
+var fievres=prompt("donner les fievres");
+if(fievres==="oui"){
+    alert("Rhume");
+}
+else{
+    alert("Refroidissement");
+}
+
